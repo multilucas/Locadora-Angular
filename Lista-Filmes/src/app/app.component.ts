9d833af5fcf026c6dfc85e51edb939a8b03db4ad
lucas <lucaslopes198@gmail.com>
2023-11-24 21:28:45 -0300

    alterando estilos

--- Lista-Filmes/src/app/app.component.ts
+++ Lista-Filmes/src/app/app.component.ts
@@ -10,18 +10,25 @@ import { FormsModule } from '@angular/forms';
 })
 export class AppComponent {
   filmes = [
-    { nome: 'Filme 1', categoria: 'Ação' },
-    { nome: 'Filme 2', categoria: 'Comédia' },
-    { nome: 'Filme 3', categoria: 'Drama' },
-    { nome: 'Filme 1', categoria: 'Ação' },
-    { nome: 'Filme 2', categoria: 'Comédia' },
-    { nome: 'Filme 3', categoria: 'Drama' },
-    { nome: 'Filme 1', categoria: 'Ação' },
-    { nome: 'Filme 2', categoria: 'Comédia' },
-    { nome: 'Filme 3', categoria: 'Drama' },
-    { nome: 'Filme 1', categoria: 'Ação' },
-    { nome: 'Filme 2', categoria: 'Comédia' },
-    { nome: 'Filme 3', categoria: 'Drama' },
+    { nome: 'Ação no Ar', categoria: 'Ação' },
+    { nome: 'Corrida Final', categoria: 'Ação' },
+    { nome: 'Missão Impossível', categoria: 'Ação' },
+    { nome: 'Risada Certa', categoria: 'Comédia' },
+    { nome: 'Férias Frustradas', categoria: 'Comédia' },
+    { nome: 'Noite de Jogo', categoria: 'Comédia' },
+    { nome: 'Lágrimas do Sol', categoria: 'Drama' },
+    { nome: 'O Pianista', categoria: 'Drama' },
+    { nome: 'Um Sonho de Liberdade', categoria: 'Drama' },
+    { nome: 'Aventura Espacial', categoria: 'Ação' },
+    { nome: 'Perseguição Implacável', categoria: 'Ação' },
+    { nome: 'Operação Resgate', categoria: 'Ação' },
+    { nome: 'Riso Contagioso', categoria: 'Comédia' },
+    { nome: 'Festa Maluca', categoria: 'Comédia' },
+    { nome: 'Encontro de Risos', categoria: 'Comédia' },
+    { nome: 'Caminho da Esperança', categoria: 'Drama' },
+    { nome: 'Luz no Fim', categoria: 'Drama' },
+    { nome: 'Destino Incerto', categoria: 'Drama' },
+    { nome: 'Escolhas da Vida', categoria: 'Drama' },
   ];
 
   categoriaSelecionada = '';
